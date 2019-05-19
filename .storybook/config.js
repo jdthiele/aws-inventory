7102745826f0c964600001b3d8efbb8fce4ea7af
David Thiele <jdthiele@gmail.com>
2019-05-19 02:26:20 -0500

    testing out a react-spa

---
+++ .storybook/config.js
@@ -0,0 +1,10 @@
+// .storybook/config.js
+import { configure } from '@storybook/react';
+
+const req = require.context('../components', true, /stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(req)
+}
+
+configure(loadStories, module);
---
+++ components/atoms/Button/index.js
@@ -0,0 +1,20 @@
+// components/atoms/Button/index.js
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button as MaterialButton } from '@material-ui/core';
+
+const Button = (props) => {
+  const { children, ...defaultProps } = props;
+
+  return (
+    <MaterialButton {...defaultProps}>
+      {children}
+    </MaterialButton>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Button;
